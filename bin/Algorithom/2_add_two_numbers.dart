b10f3aa91c6a62bfe7d84ea9abb0928ca57f5142
Arafat <arafat2k30@gmail.com>
2025-05-06 17:51:02 +0600

    slove linkList problem

--- bin/Algorithom/2_add_two_numbers.dart
+++ bin/Algorithom/2_add_two_numbers.dart
@@ -1,33 +1,68 @@
 void main(){
-  List<int> num1= [2,4,3];
-   final l1= num1.reversed.toList();
 
-  List<int> num2= [5,6,4];
-  final l2= num2.reversed.toList();
+  List<int> l1= [2,3,4];
+  List<int> l2= [5,6,7];
 
+  ListNode? node1 =buildList(l1);
+  ListNode? node2 =buildList(l2);
+  ListNode? result = Solution().addTwoNumbers(node1, node2);
 
-  List<int> number = addTwonumber(l1, l2);
-  print(number);
+  printList(result);
 
 }
 
-List<int>addTwonumber(List<int>l1, List<int>l2){
-  List<int> results =[];
- int carry = 0;
 
- int maxlength = l1.length > l2.length ? l1.length :l2.length;
- for( int i =0 ; i<maxlength ; i++ ){
-   int x= i<l1.length ? l1[i] : 0;
-   int y= i<l2.length ? l2[i] : 0;
+class ListNode{
+  int val ;
+  ListNode?next ;
+  ListNode([this.val =0 , this.next]);
 
-   int sum = x+y+carry ;
-   results.add(sum % 10);
-   carry = sum ~/10;
+}
+
+ class Solution{
+
+
+  ListNode?addTwoNumbers(ListNode?l1 , ListNode?l2){
+    ListNode dummyHead = ListNode(0);
+    ListNode current = dummyHead;
+    int carry = 0;
+
+    while(l1 !=null || l2!=null ||carry !=0 ){
+      int val1=l1?.val?? 0;
+      int val2=l2?.val?? 0;
+
+      int sum = val1+val2+carry;
+      carry = sum~/10;
+      int digit =sum%10;
+
+      current.next= ListNode(digit);
+      current = current.next!;
+
+      l1 =l1?.next;
+      l2 = l2?.next;
+    }
+    return dummyHead.next;
+  }
+
  }
- 
- if(carry> 0){
-   results.add(carry);
- }
+// ✅ Convert List<int> to Linked List
+ListNode? buildList(List<int> list) {
+  if (list.isEmpty) return null;
+  ListNode head = ListNode(list[0]);
+  ListNode current = head;
+  for (int i = 1; i < list.length; i++) {
+    current.next = ListNode(list[i]);
+    current = current.next!;
+  }
+  return head;
+}
 
-  return results ;
+// ✅ Print linked list nicely
+void printList(ListNode? node) {
+  List<int> result = [];
+  while (node != null) {
+    result.add(node.val);
+    node = node.next;
+  }
+  print(result); // Example: [7, 9, 1]
 }
